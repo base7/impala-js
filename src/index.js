d0cc7c36829f3d2da9735f80a1818c16e64e8e13
James Herrington <james@getimpala.com>
2018-02-19 11:23:35 +0000

    fix(index.js): support instantiating with hotelId

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import * as api from './api'
-import withDefaults from './withDefaults'
+import * as api from "./api";
+import withDefaults from "./withDefaults";
 
-const Impala = function(apiKey) {
-  return {
-    ...withDefaults({ apiKey }),
-    getHotel: hotelId => {
+const Impala = function({ apiKey, hotelId }) {
+  const impala = {
+    ...withDefaults({ apiKey, hotelId })
+  };
+
+  if (!hotelId) {
+    impala.getHotel = hotelId => {
       if (!hotelId) {
-        throw new Error('getHotel requires a hotelId as its first argument')
+        throw new Error("getHotel requires a hotelId as its first argument");
       }
-      return withDefaults({ apiKey, hotelId })
-    }
+      return withDefaults({ apiKey, hotelId });
+    };
   }
-}
 
-export default Impala
+  return impala;
+};
+
+export default Impala;
